34b9d454fe358c4ce776e14f2bd145d7dddd4247
nilFinx <nulflox@disroot.org>
2026-05-24 17:55:25 +0900

    no spoiler in my house /j

--- Skymu/Skyaeris/Main.xaml.cs
+++ Skymu/Skyaeris/Main.xaml.cs
@@ -1013,7 +1013,7 @@ namespace Skymu.Skyaeris
 
         private void OnOptions(object sender, RoutedEventArgs e)
         {
-            new Views.OptionsNew("Background").Show();
+            new Views.Options("Background").Show();
         }
 
         private void OnAbout(object sender, RoutedEventArgs e)
