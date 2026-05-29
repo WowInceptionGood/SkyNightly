1d47bae4400a7e1fd76eaf5e00ee2930547f8795
nilFinx <nulflox@disroot.org>
2026-05-29 10:47:07 +0900

    Import police

--- Skymu/Classes/Enumerations.cs
+++ Skymu/Classes/Enumerations.cs
@@ -9,12 +9,6 @@
 // License: https://skymu.app/legal/license
 /*==========================================================*/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Skymu.Enumerations
 {
     public enum WindowFrame
